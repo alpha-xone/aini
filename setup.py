0e72b01045fd0176d3a03e0e8ebb8ba3ddf6f5bb
alpha-xone <alpha.xone@outlook.com>
2025-05-09 04:22:07 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if os.path.exists('requirements.txt'):
 
 setup(
     name='aini',
-    version='0.1.0',
+    version='0.1.1',
     author='Alpha x1',
     author_email='alpha.xone@outlook.com',
     description='Make class instantiation easy with auto-imports',
